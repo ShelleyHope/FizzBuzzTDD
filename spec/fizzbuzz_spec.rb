cab4ec15ae661c1049559ac8f99fda0a3c0f1f07
Shelley Hope <shelleyjhope@gmail.com>
2014-07-09 10:43:22 +0100

    second failing test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,8 @@ describe 'FizzBuzz' do
   it 'knows 3 is divisible by 3' do
     expect(is_divisible_by_three(3)).to be true
   end
-  
-  
+
+  it 'knows 1 is not divisible by 3' do
+    expect(is_divisible_by_three(1)).to be false
+  end
 end
